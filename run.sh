5bf3e377b68319f547045edd2c9e8880c3b6066e
88plug <19512127+88plug@users.noreply.github.com>
2024-04-06 16:52:44 -0700

    Update run.sh
    Pull latest farmer and detect Broadwell, Haswell, Epyc, and Ryzen.

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Hello, $plots plots requested"
+echo "Hello, $plots plots requested to $reward_address"
+
+echo "Checking CPU requested"
+if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ]; then
+CPU_COUNT=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+else
+CPU_COUNT=$(cat /sys/fs/cgroup/cpu.max | awk '{print $1}')
+fi
+CPU_COUNT=$(echo "scale=0; $CPU_COUNT/100000" | bc -l) #Convert to Cores
+echo "Found $CPU_COUNT cpus available."
+
+
 #Setup filemanager
 mkdir /plots
 chmod 777 /plots -R
@@ -10,22 +21,63 @@ mv /config.php /plots/
 mv /nginx.conf /etc/nginx/sites-enabled/default
 mv /nginx-default.conf /etc/nginx/nginx.conf
 
-sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/8.1/fpm/php.ini
-sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 1000G/g" /etc/php/8.1/fpm/php.ini
-sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 1000G/g" /etc/php/8.1/fpm/php.ini
-sed -i -e "/listen\s*=\s*\/run\/php\/php8.1-fpm.sock/c\listen = 127.0.0.1:9000" /etc/php/8.1/fpm/pool.d/www.conf
-sed -i -e "/pid\s*=\s*\/run/c\pid = /run/php8.1-fpm.pid" /etc/php/8.1/fpm/php-fpm.conf
+sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/8.3/fpm/php.ini
+sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 1000G/g" /etc/php/8.3/fpm/php.ini
+sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 1000G/g" /etc/php/8.3/fpm/php.ini
+sed -i -e "/listen\s*=\s*\/run\/php\/php8.3-fpm.sock/c\listen = 127.0.0.1:9000" /etc/php/8.3/fpm/pool.d/www.conf
+sed -i -e "/pid\s*=\s*\/run/c\pid = /run/php8.3-fpm.pid" /etc/php/8.3/fpm/php-fpm.conf
 #Adjusting app name
 sed -i -e "s/File Manager/Subspace Plot Manager/g" /plots/index.php
-sed -i -e "s/Tiny Chia Plot Manager/Subspace Plot Manager/g" /plots/index.php
 
 /etc/init.d/nginx start
-/etc/init.d/php8.1-fpm start
+/etc/init.d/php8.3-fpm start
 
 echo "Using $plots plots"
 
+echo "Fetching the latest release from GitHub..."
+#latest_release=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r '.assets[] | select(.name | contains("farmer") and contains("v2") and contains("ubuntu") and contains("x86_64")) | .browser_download_url' | head -n1)
+#latest_release=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r '.assets[] | select(.name | contains("farmer") and contains("ubuntu") and contains("x86_64")) | .browser_download_url' | head -n1)
+
+# Identify CPU model
+cpu_model=$(lscpu | grep "Model name" | cut -d ':' -f2 | xargs)
+
+echo "Detected CPU: $cpu_model"
+
+# Determine the appropriate release based on CPU model
+if [[ "$cpu_model" == *"Ryzen"* || "$cpu_model" == *"EPYC"* || "$cpu_model" == *"Skylake"* ]]; then
+  echo "Fetching the latest release for Ryzen from GitHub..."
+  latest_release=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r '.assets[] | select(.name | contains("farmer") and contains("ubuntu") and contains("x86_64")) | .browser_download_url' | head -n1)
+elif [[ "$cpu_model" == *"Broadwell"* || "$cpu_model" == *"Haswell"* ]]; then
+  echo "Fetching the latest V2 release for Broadwell from GitHub..."
+  latest_release=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r '.assets[] | select(.name | contains("farmer") and contains("v2") and contains("ubuntu") and contains("x86_64")) | .browser_download_url' | head -n1)
+else
+  echo "No specific release found for $cpu_model. Using failsafe v2"
+  latest_release=$(curl -s https://api.github.com/repos/subspace/subspace/releases/latest | jq -r '.assets[] | select(.name | contains("farmer") and contains("v2") and contains("ubuntu") and contains("x86_64")) | .browser_download_url' | head -n1)
+fi
+
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to fetch the latest release from GitHub. Please check your internet connection and try again."
+  exit
+fi
+echo "Latest release URL: $latest_release"
+
+executable=$(echo $latest_release | awk -F'/' '{print $NF}')
+echo "Using $executable"
+
+echo "Downloading the latest release..."
+wget $latest_release
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to download the latest release. Please check your internet connection and try again."
+  exit
+fi
+echo "Latest release : $latest_release downloaded successfully."
+
+echo "Setting executable permissions on the binary..."
+chmod +x $executable
+echo "Executable permissions set successfully."
+
 # Define base parameters
-base_command="./subspace-farmer-ubuntu-x86_64-v2-gemini-3h-2024-feb-19 farm --farm-during-initial-plotting=true --node-rpc-url ws://node:9944 --listen-on /ip4/0.0.0.0/udp/30533/quic-v1 --listen-on /ip4/0.0.0.0/tcp/30533 --reward-address \$reward_address"
+base_command="./${executable} farm --allow-private-ips --node-rpc-url ws://node:9944 --listen-on /ip4/0.0.0.0/udp/30533/quic-v1 --listen-on /ip4/0.0.0.0/tcp/30533 --reward-address $reward_address"
 path_base="/plots/plot"
 
 # Create directories for each plot
